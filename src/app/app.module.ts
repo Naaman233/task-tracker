6ea319fbc743ef40a4e5bd40e58d35df4b2ec69e
Makafui Naaman <makafuinaaman@Makafuis-MacBook-Pro.local>
2020-06-14 01:35:40 -0500

    Implemented UI functionality to read data from the client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule , ReactiveFormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -16,7 +16,9 @@ import { SignUpComponent } from './sign-up/sign-up.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
-import{Router} from '@angular/router'
+import {Router} from '@angular/router'
+import {FormControl,FormGroup} from '@angular/forms'
+
 
 @Component({
   selector: 'app-sign-up',
@@ -8,15 +10,20 @@ import{Router} from '@angular/router'
   styleUrls: ['./sign-up.component.css']
 })
 
-
 export class SignUpComponent implements OnInit {
   errorMessage;
   constructor(private authService: AuthenticationService, private router:Router) { }
+
+  loginForm = new FormGroup({
+    inputEmail : new FormControl(''),
+    inputPassword : new FormControl('')
+  })
 async onSubmitClick(event){
   event.preventDefault();
-  console.log('button clicked')
+  console.log(this.loginForm.value)
+ 
   try{
-    await this.authService.signUpUser('test@test1.com','123456789')
+    await this.authService.signUpUser(this.loginForm.value)
     this.router.navigateByUrl('/sign-in')
   }catch(error){
     console.log(error);
